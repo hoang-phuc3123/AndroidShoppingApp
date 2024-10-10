c9913bf323f5866b3c39eae241ff07d5c1ba6c59
Đỗ Lê Quý Phúc <phucdlq@gmail.com>
2024-10-10 17:39:39 +0700

    add: product list, product detail, filter, sort

--- app/src/main/java/com/project/mainprojectprm231/models/Product.java
+++ app/src/main/java/com/project/mainprojectprm231/models/Product.java
@@ -5,12 +5,18 @@ public class Product {
     private String imageUrl;
     private double price;
     private String description;
+    private String id;
+    private String brand;
+    private int rating;
 
-    public Product(String name, String imageUrl, double price, String description) {
+    public Product(String name, String imageUrl, double price, String description, String id, String brand, int rating) {
         this.name = name;
         this.imageUrl = imageUrl;
         this.price = price;
         this.description = description;
+        this.id = id;
+        this.brand = brand;
+        this.rating = rating;
     }
 
     public String getName() {
@@ -43,6 +49,30 @@ public class Product {
 
     public void setDescription(String description) {
         this.description = description;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getBrand() {
+        return brand;
+    }
+
+    public void setBrand(String brand) {
+        this.brand = brand;
+    }
+
+    public int getRating() {
+        return rating;
+    }
+
+    public void setRating(int rating) {
+        this.rating = rating;
     }
 }
 
