de3cf01798646d5c440c6d9f880f1e8bc414a8ec
Đỗ Lê Quý Phúc <phucdlq@gmail.com>
2024-10-19 20:43:49 +0700

    add: product detail, profile screen

--- app/src/main/java/com/project/mainprojectprm231/networking/ApiClient.java
+++ app/src/main/java/com/project/mainprojectprm231/networking/ApiClient.java
@@ -3,14 +3,49 @@ package com.project.mainprojectprm231.networking;
 import okhttp3.Callback;
 import okhttp3.OkHttpClient;
 import okhttp3.Request;
+import okhttp3.RequestBody;
+import okhttp3.MediaType;
+
+import org.json.JSONObject;
 
 public class ApiClient {
-    private static final String BASE_URL = "https://shop-mobile-api.opalwed.id.vn/api/v1/product";
+    private static final String BASE_URL = "https://shop-mobile-api.opalwed.id.vn/api/v1";
     private static final OkHttpClient client = new OkHttpClient();
 
     public static void fetchProducts(Callback callback) {
         Request request = new Request.Builder()
-                .url(BASE_URL)
+                .url(BASE_URL + "/product")
+                .build();
+
+        client.newCall(request).enqueue(callback);
+    }
+
+    public static void registerUser(String email, String username, String password, String phone, String address, Callback callback) {
+        MediaType JSON = MediaType.parse("application/json; charset=utf-8");
+        JSONObject jsonObject = new JSONObject();
+        try {
+            jsonObject.put("email", email);
+            jsonObject.put("username", username);
+            jsonObject.put("password", password);
+            jsonObject.put("phone", phone);
+            jsonObject.put("address", address);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        RequestBody body = RequestBody.create(JSON, jsonObject.toString());
+        Request request = new Request.Builder()
+                .url(BASE_URL + "/user")
+                .post(body)
+                .build();
+
+        client.newCall(request).enqueue(callback);
+    }
+
+    public static void getUserData(String uid, Callback callback) {
+        String url = BASE_URL + "/user?UID=" + uid;
+        Request request = new Request.Builder()
+                .url(url)
                 .build();
 
         client.newCall(request).enqueue(callback);
